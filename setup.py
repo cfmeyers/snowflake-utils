16b68b89bbcf4b355c5e149d358bf3a017b002bb
Collin Meyers <cfmeyers@gmail.com>
2019-03-13 07:03:20 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+install_requires = ['snowflake-connector-python']
+setup(
+    name='SnowflakeUtils',
+    version='0.1',
+    packages=['snowflake_utils'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    install_requires=install_requires,
+)
