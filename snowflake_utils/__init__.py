b88080fe712234da5bc7ee721d632afbb2697942
Collin Meyers <cfmeyers@gmail.com>
2019-03-16 12:34:11 -0400

    Make functions importable

--- snowflake_utils/__init__.py
+++ snowflake_utils/__init__.py
@@ -1,1 +1,6 @@
-from .snowflake_utils import get_dict_cursor, get_results_from_query
+from .snowflake_utils import (
+    get_dict_cursor,
+    get_results_from_query,
+    get_dict_cursor_from_connection,
+    get_sf_connection,
+)
