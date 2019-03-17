e295ed93a0e2a67e96d9abf79c0cf16b6d5a952c
Collin Meyers <cfmeyers@gmail.com>
2019-03-16 22:05:05 -0400

    Specify connection creds using .snowsql/config header

--- snowflake_utils/snowflake_utils.py
+++ snowflake_utils/snowflake_utils.py
@@ -6,22 +6,18 @@ import snowflake.connector
 from snowflake.connector import DictCursor
 
 
-path_to_snowflake_config = os.path.expanduser('~/.snowsql/config')
-config = configparser.ConfigParser()
-config.read(path_to_snowflake_config)
-ACCOUNT = config['connections.prod_etl']['accountname']
-USER = config['connections.prod_etl']['username']
-PASSWORD = config['connections.prod_etl']['password']
-
-
-def get_sf_connection(database: str = 'RTR_QA', role: str = 'ANALYST_ROLE'):
+def get_sf_connection(connection_name: str):
+    path_to_snowflake_config = os.path.expanduser('~/.snowsql/config')
+    config = configparser.ConfigParser()
+    config.read(path_to_snowflake_config)
+    creds = config[connection_name]
     con = snowflake.connector.connect(
-        user=USER,
-        password=PASSWORD,
-        account=ACCOUNT,
-        database=database,
-        warehouse='ANALYST_WAREHOUSE',
-        role=role,
+        user=creds['username'],
+        password=creds['password'],
+        account=creds['accountname'],
+        database=creds['dbname'],
+        warehouse=creds['warehousename'],
+        role=creds['rolename'],
     )
     return con
 
@@ -30,8 +26,8 @@ def get_dict_cursor_from_connection(con) -> DictCursor:
     return con.cursor(DictCursor)
 
 
-def get_dict_cursor(database: str = 'RTR_QA', role: str = 'ANALYST_ROLE') -> DictCursor:
-    con = get_sf_connection(database, role)
+def get_dict_cursor(connection_name: str) -> DictCursor:
+    con = get_sf_connection(connection_name, role)
     return con.cursor(DictCursor)
 
 
